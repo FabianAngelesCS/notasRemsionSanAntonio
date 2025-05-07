1fb89e62f76ef94684112cbe1d2324913dd9db90
SebazRockdz <sebazrdz@gmail.com>
2025-05-06 21:43:37 -0600

    creacion de formulario producto y funcion de boton limpiar

--- src/main/java/com/mycompany/notasremisionsanantonio/persistencia/Conexion.java
+++ src/main/java/com/mycompany/notasremisionsanantonio/persistencia/Conexion.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexion {
     private static final String URL = "jdbc:mysql://localhost:3306/SanAntonioRemision";
     private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "2856";
 
     public Connection conectar() {
         try {
