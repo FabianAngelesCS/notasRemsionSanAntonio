ba3386ef41ab14348c616d641599903ca4f5425d
scorp <scorp@FabianCS.mshome.net>
2025-07-04 08:08:09 -0600

    probar modificar

--- src/main/java/com/mycompany/notasremisionsanantonio/persistencia/Conexion.java
+++ src/main/java/com/mycompany/notasremisionsanantonio/persistencia/Conexion.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/SanAntonioRemision";
+    private static final String URL = "jdbc:mysql://localhost:3307/SanAntonioRemision";
     private static final String USER = "root";
-    private static final String PASSWORD = "2856";
+    private static final String PASSWORD = "root";
 
     public Connection conectar() {
         try {
